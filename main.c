a58704a2edc59d5ea4498a5819f8fb8e9e9664ce
arturcunha20 <arturcunhagpsi1720@gmail.com>
2021-01-05 15:59:59 +0000

    Merge remote-tracking branch 'origin/main' into main

--- main.c
+++ main.c
@@ -635,6 +635,6 @@ int main() {
             case 0: printf("Ate uma proxima");break;
         }
     } while (opcao < 1 && opcao > 4 || opcao != 0);
-    printf("Eu sou bue fixe 12345");
+    printf("O leandro é boiola");
     return 0;
 }
